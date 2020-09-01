be011f5477c843008b67da65be8e22ace64da739
rfoldbirk <rasmusfoldberg@gmail.com>
2020-09-01 22:13:52 +0200

    Tilføjede alacritty configuration

--- copy_nvim.sh
+++
@@ -1,4 +0,0 @@
-mkdir $HOME/.config
-mkdir $HOME/.config/nvim
-
-cp $HOME/.dotfiles/nvim $HOME/.config/nvim
---
+++ nvim_alacritty.sh
@@ -0,0 +1,6 @@
+mkdir $HOME/.config
+mkdir $HOME/.config/nvim
+mkdir $HOME/.config/alacritty
+
+cp $HOME/.dotfiles/nvim $HOME/.config/
+cp $HOME/.dotfiles/alacritty.yml $HOME/.config/alacritty/
