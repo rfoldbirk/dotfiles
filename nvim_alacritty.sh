617b845a4c830b74478127abf1038f559821eb83
rfoldbirk <rasmusfoldberg@gmail.com>
2020-09-01 22:16:59 +0200

    fiksede cp

--- nvim_alacritty.sh
+++ nvim_alacritty.sh
@@ -2,5 +2,5 @@ mkdir $HOME/.config
 mkdir $HOME/.config/nvim
 mkdir $HOME/.config/alacritty
 
-cp $HOME/.dotfiles/nvim $HOME/.config/
-cp $HOME/.dotfiles/alacritty.yml $HOME/.config/alacritty/
+cp -R $HOME/.dotfiles/nvim $HOME/.config/
+cp -R $HOME/.dotfiles/alacritty.yml $HOME/.config/alacritty/
